b33bfc040aa2a2ff3c69b38d2ff4a677412f090f
Pol <pacadi92@gmail.com>
2022-03-09 11:04:42 +0100

    final commit
    added an executable file and modified the software, now it calculates km to miles

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 A basic miles to kilometres conversor made with tkinter
+
+You can py run it or just use the executable.
+
+If you don't trust this executable but want to try it out anyway you can run the python file yourself:  
+
+1) Install python, see how in:  [https://www.python.org/downloads/]
+
+2) Run it
--- main.py
+++ main.py
@@ -1,37 +1,37 @@
 from tkinter import *
 
 
-def miles_to_km():
-    miles = float(miles_input.get())
-    km = miles * 1.609
-    kilometer_result_label.config(text=f"{km}")
+def km_to_miles():
+    km = float(km_input.get())
+    miles = 0.6213711 * km
+    kilometer_result_label.config(text=f"{miles}")
 
 
 # Main window
 
 window = Tk()
-window.title("Miles to Kilometer Converter")
-window.config(padx=20, pady=20)
+window.title("Kilometers to Miles converter by Geloon")
+window.config(padx=120, pady=50)
 
 # Input
 
-miles_input = Entry(width=7)
-miles_input.grid(column=1, row=0)
+km_input = Entry(width=7)
+km_input.grid(column=1, row=0)
 
 # Labels
 
-miles_label = Label(text="Miles")
-miles_label.grid(column=2, row=0)
+kilometers_label = Label(text="Km")
+kilometers_label.grid(column=2, row=0)
 is_equal_label = Label(text="is equal to")
 is_equal_label.grid(column=0, row=1)
 kilometer_result_label = Label(text="0")
 kilometer_result_label.grid(column=1, row=1)
-kilometer_label = Label(text="Km")
-kilometer_label.grid(column=2, row=1)
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=1)
 
 # Button
 
-calculate_button = Button(text="Calculate", command=miles_to_km)
+calculate_button = Button(text="Calculate", command=km_to_miles)
 calculate_button.grid(column=1, row=2)
 
 window.mainloop()
